af62b6a63c9e66a1790b7e5cc5092a5f80021096
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-01-19 01:56:54 +0600

    Add files via upload

---
+++ infix2prefix.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include<stack>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isOperator(char c)
+{
+    return (c=='+' || c=='-' || c=='*' || c=='/' || c=='^');
+}
+
+int precedence(char c)
+{
+    if(c=='^') return 3;
+    else if(c=='*' || c=='/') return 2;
+    else if(c=='+' || c=='-') return 1;
+    else return -1;
+}
+
+string infix2postfix(stack<char> &s, const string& infix)
+{
+    string prefix;
+    for(int i=0; i<infix.length(); i++)
+    {
+        char c=infix[i];
+        if(isalnum(c))
+        {
+            prefix+=c;
+        }
+        else if(c=='(')
+        {
+            s.push(c);
+        }
+        else if(c==')')
+        {
+            while((s.top()!='(') && (!s.empty()))
+            {
+                prefix+=s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+        else if(isOperator(c))
+        {
+            while(!s.empty() && precedence(c)<precedence(s.top()) && !(c=='^' && s.top()=='^'))
+            {
+                prefix+=s.top();
+                s.pop();
+            }
+            s.push(c);
+        }
+    }
+        while(!s.empty())
+        {
+            prefix+=s.top();
+            s.pop();
+        }
+        return prefix;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    string infix,prefix;
+    cin>>infix;
+    stack <char> s;
+    reverse(infix.begin(),infix.end());
+
+    for(int i=0; i<infix.length(); i++)
+    {
+        if(infix[i]=='(') infix[i]=')';
+        else if(infix[i]==')') infix[i]='(';
+    }
+    prefix=infix2postfix(s,infix);
+    reverse(prefix.begin(),prefix.end());
+
+
+    cout<<prefix<<endl;
+    return 0;
+}
+
+
