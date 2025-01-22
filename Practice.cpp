6146dd988f03765cd586ebe8635f2272b867a214
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-01-23 01:15:01 +0600

    Graph_dfs().cpp

--- Practice.cpp
+++ Practice.cpp
@@ -1,90 +1,40 @@
-#include <iostream>
-#include<stack>
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-class graph{
-    int a[100][100],n,s;
-public:
-    void getdata();
-    void bfs();
-    void dfs();
-};
-
-void graph::getdata()
+void dfs(const vector<vector<int>>& adjMatrix, int node, vector<bool> &visited)
 {
-    cout<<"Enter number of nodes in the graph :"<<endl;
-    cin>>n;
-    cout<<"Enter adjacency matrix : "<<endl;
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-          cin>>a[i][j];
-    cout<<"Node to traverse from : "<<endl;
-    cin>>s;
-}
+    cout<<node+1<< " ";
 
-void graph::bfs()
-{
-    int* visited=new int[n];
-    int queue[10], front=-1, i, rear=0;
+    visited[node]=true;
 
-    for(int j=0; j<n; j++)
-        visited[j]=0;
-
-    queue[rear]=s;
-    visited[s]=1;
-
-    while(front!=rear)
-    {
-        cout<<queue[++front]<<" ";
-        i=queue[front];
-
-        for(int j=0; j<n; j++)
+    for(int i=0; i<adjMatrix[node].size(); i++)
         {
-            if(a[i][j]!=0 && visited[j]!=1)
-            {
-                queue[++rear]=j;
-                visited[j]=1;
-            }
+        if(adjMatrix[node][i]==1 && !visited[i])
+        {
+            dfs(adjMatrix,i,visited);
         }
     }
 }
 
-void graph::dfs()
-{
-   int* visited=new int[n];
-    int stack[10], top=-1, i;
-
-    for(int j=0; j<n; j++)
-        visited[j]=0;
-
-        i=stack[++top]=s;
-        visited[s]=1;
-
-        while(top>=0)
-        {
-           i=stack[top];
-           cout<<stack[--top]<<" ";
-
-           for(int j=n-1; j>=0; j--)
-           {
-               if(a[i][j]!=0 && visited[j]!=1)
-               {
-                   stack[++top]=j;
-                   visited[j]=1;
-               }
-           }
-
-        }
-}
-
-
 int main()
 {
-    graph g;
-    g.getdata();
-    g.bfs();
-    g.getdata();
-    g.dfs();
+    vector<vector<int>> adjMatrix = {
+        {0, 1, 1, 0, 0, 0, 0},
+        {1, 0, 0, 1, 1, 0, 0},
+        {1, 0, 0, 0, 0, 1, 1},
+        {0, 1, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0}
+};
+
+    int nodes = adjMatrix.size();
+    vector<bool> visited(nodes, false);
+
+    cout<<"DFS Traversal: ";
+    dfs(adjMatrix, 0, visited);
+    cout<<endl;
+
     return 0;
 }
