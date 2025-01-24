b68933105354b056fc7fbdd9ba8f815c902a3833
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-01-24 21:50:24 +0600

    Create sorting_optimizedBubbleSort().cpp

---
+++ sorting_optimizedBubbleSort().cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter array size : ";
+    int n; cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+
+    for(int i=n-1,swp=1; i>0 && swp; i--)
+    {
+        swp=0;
+        for(int j=0; j<i; j++)
+        if(arr[j]>arr[j+1])
+        {
+            swap(arr[j],arr[j+1]);
+            swp=1;
+        }
+    }
+
+        cout<<"Sorted array : ";
+        for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+
+    return 0;
+}
+
